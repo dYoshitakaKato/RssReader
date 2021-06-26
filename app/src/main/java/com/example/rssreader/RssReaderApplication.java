ce5a59232e4be8053d105289f3bcf89107f064e2
dYoshitakaKato <phoenix1331019@gmail.com>
2021-06-26 22:03:26 +0900

    アプリケーションの追加

---
+++ app/src/main/java/com/example/rssreader/RssReaderApplication.java
@@ -0,0 +1,9 @@
+package com.example.rssreader;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class RssReaderApplication extends Application {
+}
