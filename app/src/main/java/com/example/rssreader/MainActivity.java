21b14b1f707e346234c367793f53e7e89acfe2fb
dYoshitakaKato <phoenix1331019@gmail.com>
2021-06-28 03:30:17 +0900

    カテゴリ選択時にドロワーを閉じる対応

--- app/src/main/java/com/example/rssreader/MainActivity.java
+++ app/src/main/java/com/example/rssreader/MainActivity.java
@@ -56,10 +56,11 @@ public class MainActivity extends AppCompatActivity {
         CategoryDialogFragment.newInstance().show(getSupportFragmentManager(), CategoryDialogFragment.class.getName());
     }
 
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-//                || super.onSupportNavigateUp();
-//    }
+    @Override
+    public void onSelectedCategory(CategoryData category) {
+        if (mBinding == null) {
+            return;
+        }
+        mBinding.drawerLayout.closeDrawers();
+    }
 }
---
+++ app/src/main/java/com/example/rssreader/ui/CategoryAddClickListener.java
@@ -0,0 +1,7 @@
+package com.example.rssreader.ui;
+
+import com.example.rssreader.entities.CategoryData;
+
+public interface CategoryAddClickListener {
+    void onCategoryAddClick(CategoryData data);
+}
