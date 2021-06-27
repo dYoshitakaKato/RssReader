c3a5322083d6245307bc6d994d1a82cc898f974f
dYoshitakaKato <phoenix1331019@gmail.com>
2021-06-28 03:43:03 +0900

    リファクター

--- app/src/main/java/com/example/rssreader/MainActivity.java
+++ app/src/main/java/com/example/rssreader/MainActivity.java
@@ -1,29 +1,24 @@
 package com.example.rssreader;
 
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 
 import com.example.rssreader.databinding.ActivityMainBinding;
 import com.example.rssreader.entities.CategoryData;
-import com.example.rssreader.ui.ItemClickListener;
+import com.example.rssreader.ui.CategorySelectListener;
 import com.example.rssreader.ui.article.list.ArticlesFragment;
 import com.example.rssreader.ui.category.dialog.CategoryDialogFragment;
 import com.example.rssreader.ui.category.list.CategoryRecycleFragment;
-import com.google.android.material.navigation.NavigationView;
 
-import androidx.fragment.app.Fragment;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CategorySelectListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
-
+    private ActivityMainBinding mBinding;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,22 +29,13 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.container, ArticlesFragment.newInstance(0)).commitNow();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mBinding.getRoot();
         setContentView(view);
-        binding.setLifecycleOwner(this);
-        binding.setOwner(this);
+        mBinding.setLifecycleOwner(this);
+        mBinding.setOwner(this);
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.recycle_fragment, CategoryRecycleFragment.newInstance()).commitNow();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     public void onClick(){
